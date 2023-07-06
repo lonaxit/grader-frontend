1b9062835c3fbcee4e1343eb10965216a13286f0
lonaxit <lonaxit@outlook.com>
2023-07-06 11:06:25 +0100

    Setup update
    Setup update

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/css/bootstrap.min.css'
+import './assets/lib/owlcarousel/assets/owl.carousel.min.css'
+import './assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css'
 
 createApp(App).use(store).use(router).mount('#app')
