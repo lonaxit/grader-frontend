7a498bba52d632d1c6e9b68b413858fdbb345375
lonaxit <lonaxit@outlook.com>
2023-07-20 08:11:22 +0100

    Update Sidebar.vue
    Updated side bar

--- src/components/Nav/Sidebar.vue
+++ src/components/Nav/Sidebar.vue
@@ -111,7 +111,7 @@
                     </div>
 
                      <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-users me-2"></i>Enrollment</a>
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Enrollment</a>
                         <div class="dropdown-menu bg-transparent border-0">
                             <router-link to='/new-enrollment/' class="dropdown-item">New</router-link>
                             <router-link to='/find-enrollment/' class="dropdown-item">Roll Call</router-link>
@@ -121,7 +121,7 @@
 
 
                       <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-users me-2"></i>Grading</a>
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-th me-2"></i>Grading</a>
                         <div class="dropdown-menu bg-transparent border-0">
                           
                             <!-- <router-link to='/export-sheet/' class="dropdown-item">Export Sheet</router-link> -->
@@ -150,30 +150,11 @@
                         </div>
                     </div>
 
-                        
-                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Elements</a>
-                        <div class="dropdown-menu bg-transparent border-0">
-                            <a href="button.html" class="dropdown-item">Buttons</a>
-                            <a href="typography.html" class="dropdown-item">Typography</a>
-                            <a href="element.html" class="dropdown-item">Other Elements</a>
-                        </div>
-                    </div>
+                
 
 
-                    <a href="widget.html" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Widgets</a>
-                    <a href="form.html" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a>
-                    <a href="table.html" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Tables</a>
-                    <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a>
-                    <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="far fa-file-alt me-2"></i>Pages</a>
-                        <div class="dropdown-menu bg-transparent border-0">
-                            <a href="signin.html" class="dropdown-item">Sign In</a>
-                            <a href="signup.html" class="dropdown-item">Sign Up</a>
-                            <a href="404.html" class="dropdown-item">404 Error</a>
-                            <a href="blank.html" class="dropdown-item">Blank Page</a>
-                        </div>
-                    </div>
+                 
+                 
                 </div>
             </nav>
         </div>
