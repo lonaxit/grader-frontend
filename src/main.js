a992d0b19d05cfb8b3e76a9862fb7eb38b93aeae
lonaxit <lonaxit@outlook.com>
2023-07-22 13:20:55 +0100

    Update main.js
    Added base url

--- src/main.js
+++ src/main.js
@@ -9,7 +9,22 @@ import './assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css'
 import axios from 'axios'
 import Notifications from '@kyvg/vue3-notification'
 
+// local
+// axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = 'https://graderapi-production.up.railway.app/'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+// router.beforeEach((to, from, next) => {
+//     const isLoggedIn = localStorage.getItem('token')
+//     if (to.matched.some(record => record.meta.requiresAuth)) { // check if the route requires authentication
+//       if (isLoggedIn) { // check if the user is logged in
+//         store.commit('initializeStore')
+//         next() // allow access to the route
+//       } else {
+//         next('/') // redirect to the public page if the user is not logged in
+//       }
+//     } else {
+//       next() // allow access to the route if authentication is not required
+//     }
+//   })
 
 createApp(App).use(Notifications).use(store).use(router,axios).mount('#app')
