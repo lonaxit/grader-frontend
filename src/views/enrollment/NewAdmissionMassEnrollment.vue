850565fc8215e589209fb7a8669f8869dc9a695f
lonaxit <lonaxit@outlook.com>
2023-12-01 19:24:18 +0100

    New admission  enrollment template

---
+++ src/views/enrollment/NewAdmissionMassEnrollment.vue
@@ -0,0 +1,149 @@
+<template>
+<div class="col-sm-12 col-xl-8">
+  <h4>New Admission Mass Enrollment</h4>
+      <div class="bg-light rounded h-100 p-4">
+
+                            <form @submit.prevent="handleSubmit">
+
+                             <div class="form-floating mb-3">
+                                <select v-model="admitted_Term" class="form-select" id="floatingSelect"
+                                    aria-label="Floating label select example">
+                                    
+                                    <option v-for="term in terms" :key="term.id" :value="term.id">{{term.name}}</option>
+                                </select>
+                                <label for="floatingSelect"> Admitted Term</label>
+                            </div>
+
+
+                                 <div class="form-floating mb-3">
+                                <select v-model="admitted_Class" class="form-select" id="floatingSelect"
+                                aria-label="Floating label select example">
+                                <option v-for="room in classes" :key="room.id" :value="room.id">{{room.class_name}}</option>
+                                </select>
+                                <label for="floatingSelect"> Admitted Class</label>
+                            </div>
+
+                                 <div class="form-floating mb-3">
+                                <select v-model="admitted_Session" class="form-select" id="floatingSelect"
+                                    aria-label="Floating label select example">
+                                    
+                                    <option v-for="session in activesess" :key="session.id" :value="session.id">{{session.name}}</option>
+                                </select>
+                                <label for="floatingSelect"> Admitted Session</label>
+                            </div> 
+
+                            <button type="submit"  :disabled="submitting" class="btn btn-primary py-3 w-100 mb-4">
+                            {{ submitting ? 'Submitting...' : 'Submit' }}
+                                </button>
+                            </form>
+                        </div>
+                 
+  </div>
+</template>
+
+<script>
+
+import {mapActions,mapMutations,mapGetters} from 'vuex'
+export default {
+      
+    data(){
+        return{
+            submitting: false,
+         
+            admitted_Class:'',
+            admitted_Session:'',
+            admitted_Term:'',
+        }
+    },
+
+    computed:{
+        ...mapGetters({sessions:'GET_SESSIONS',classes:'GET_CLASSES',terms:'GET_TERMS',activesess:'GET_ACTIVE_SESSION'})
+    },
+   
+    methods:{
+        ...mapActions(['ALL_SESSIONS','ALL_CLASSES','ALL_TERMS','FETCH_ENROLLMENT','NEW_ADMISSION_MASS_ENROLLMENT']),
+        
+        handleSubmit(){
+            this.submitting = true;
+
+                if(this.admitted_Class === ''){
+               this.submitting = false;
+                 this.$notify({
+                        title:'Error',
+                        text:'Select a class',
+                        duration:5000,
+                        type: 'error',
+                        width:'100%',
+                    })
+        
+            }
+              if(this.admitted_Session === ''){
+               this.submitting = false;
+                 this.$notify({
+                        title:'Error',
+                        text:'Select a session',
+                        duration:5000,
+                        type: 'error',
+                        width:'100%',
+                    })
+        
+            }
+
+            if(this.admitted_Term === ''){
+               this.submitting = false;
+                 this.$notify({
+                        title:'Error',
+                        text:'Select a term',
+                        duration:5000,
+                        type: 'error',
+                        width:'100%',
+                    })
+        
+            }
+
+
+            const payload={
+                newclass:this.admitted_Class,
+                newterm:this.admitted_Term,
+                newsession:this.admitted_Session,
+            }
+
+            this.NEW_ADMISSION_MASS_ENROLLMENT(payload).then((res)=>{
+                     this.submitting = false;
+                         this.$notify({
+                        title:'SUCCESS',
+                        text:'Enrollment',
+                        duration:5000,
+                        type: 'success',
+                        width:'100%',
+                    })
+            }).catch(err=>{
+                    
+                   this.submitting = false;
+                   console.log(err)
+                   this.$notify({
+                        title:'ERROR',
+                        text:err,
+                        duration:5000,
+                        type: 'error',
+                        width:'100%',
+                    })
+                })
+           
+
+            // this.$router.push({ name: 'roll-call', params: {term:this.selectedTerm,classroom: this.selectedClass, session:this.selectedSession } })  
+        }
+
+    },
+    mounted(){
+        this.ALL_TERMS()
+        this.ALL_CLASSES()
+        this.ALL_SESSIONS()
+    },
+
+}
+</script>
+
+<style>
+
+</style>
