6ab8d5aead2ae2a598fe697efebeb662a5b95bf8
lonaxit <lonaxit@outlook.com>
2024-02-19 11:11:34 +0100

    updated code

--- src/views/Result/terminalResultSummary.vue
+++ src/views/Result/terminalResultSummary.vue
@@ -163,7 +163,7 @@ export default {
     methods:{
         // ...mapActions(['DETAIL_RESULT','USER_SCORES_LIST','FETCH_TRAITS','FETCH_PSYCHOTRAITS','ALL_RESUMPTION','FETCH_ENROLLMENT']),
     //  printOnPageLoad() {
-    // window.print(); // This will open the print dialog on page load
+        //window.print(); // This will open the print dialog on page load
     // },
 
     },
