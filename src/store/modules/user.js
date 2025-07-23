c94ffe793624f0594f7bb6d360e825421cef043f
Tivkaa Manasseh Luper <lupertivkaa@gmail.com>
2025-07-23 10:35:50 +0100

    Added sorting fuctionality

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,115 +1,113 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const user = {
-    state: {
-        user: '',
-        staff:'',
-        studentusers: [],
-        allStaff:[],
-        me: '',
-        userdata: '',
-        searchedStudent:[],
+  state: {
+    user: "",
+    staff: "",
+    studentusers: [],
+    allStaff: [],
+    me: "",
+    userdata: "",
+    searchedStudent: [],
+  },
+
+  mutations: {
+    SET_USER(state, payload) {
+      state.user = payload;
+    },
+    SET_STUDENT_USERS(state, payload) {
+      state.studentusers = payload;
+    },
+    SET_STAFF(state, payload) {
+      state.staff = payload;
+    },
+    SET_ALL_STAFF(state, payload) {
+      state.allStaff = payload;
+    },
+    SET_ME(state, payload) {
+      state.me = payload;
+    },
+    SET_SAERCHEDSTUDENT(state, payload) {
+      state.searchedStudent = payload;
+    },
+  },
+
+  actions: {
+    async ADD_USER({ commit }, payload) {
+      const res = await axios.post("auth/v1/register/", payload);
     },
 
-    mutations: {
-
-        SET_USER(state, payload) {
-            state.user = payload
-        },
-        SET_STUDENT_USERS(state, payload) {
-            state.studentusers = payload
-        },
-        SET_STAFF(state, payload) {
-            state.staff = payload
-        },
-        SET_ALL_STAFF(state, payload) {
-            state.allStaff = payload
-        },
-        SET_ME(state, payload) {
-            state.me = payload
-        },
-        SET_SAERCHEDSTUDENT(state,payload){
-            state.searchedStudent = payload;
-        }
-    
-
+    async FETCH_USERS({ commit }) {
+      const res = await axios.get("auth/v1/all/users/");
+      commit("SET_STUDENT_USERS", res.data.user);
     },
 
-    actions: {
-        async ADD_USER({commit},payload) {
-            const res = await axios.post('auth/v1/register/',payload)   
-        },
-
-        async FETCH_USERS({commit}) {
-            const res = await axios.get('auth/v1/all/users/')
-            commit('SET_STUDENT_USERS',res.data.user)
-        },
-
-        async FETCH_ME({commit}) {
-            const res = await axios.get('auth/v1/me/')
-            commit('SET_ME',res.data.user)
-        },
-
-        async USER_DETAIL({commit},id) {
-            const res = await axios.get('auth/v1/user-detail/' + id +'/')
-              commit('SET_USER', res.data) 
-        },
-
-        async FETCH_ALL_STAFF({commit}) {
-            const res = await axios.get('auth/v1/all/staff/')
-            commit('SET_ALL_STAFF',res.data.user)
-        },
-
-        async STAFF_DETAIL({commit},id) {
-            const res = await axios.get('auth/v1/teacherprofile-detail/' + id +'/')
-              commit('SET_STAFF', res.data) 
-        },
-        async UPDATE_USER({commit},payload) {
-            const res = await axios.put('auth/v1/user-detail/' + payload.id +'/',payload)
-        },
-        async UPDATE_PASSWORD({commit},payload) {
-            const res = await axios.put('auth/v1/username-update-password/',payload)
-        },
-
-        // search student
-        async FIND_STUDENT({commit},payload) {
-            const res = await axios.get('api/v1/search-enroll/', {
-                params: payload,
-              })
-            commit('SET_SAERCHEDSTUDENT', res.data)
-            }
-
-       
+    async FETCH_ME({ commit }) {
+      const res = await axios.get("auth/v1/me/");
+      commit("SET_ME", res.data.user);
     },
-    getters: {
-        GET_USER(state) {
-            return state.user
-        },
-        GET_ME(state) {
-            return state.me
-        },
-    
-        GET_USERS(state) {
-        // return Array.isArray(state.studentusers) 
-        // ? state.studentusers.filter(user => user.is_student === true) 
-        // : [];
-         return state.studentusers.filter(user => user.is_student==true)
-        },
 
-        GET_NOPROFILE_STUDENTS(state) {
-            // return (state.studentusers?.filter(user => user.is_student === true && user.studentprofile === null)) || [];
-            return state.studentusers.filter(user =>  user.is_student==true && user.studentprofile===null)
-           },
+    async USER_DETAIL({ commit }, id) {
+      const res = await axios.get("auth/v1/user-detail/" + id + "/");
+      commit("SET_USER", res.data);
+    },
 
-        GET_STAFF_USERS(state) {
-            return state.allStaff.filter(user => user.is_staff==true)
-          
-        },
-        GET_STAFF(state) {
-            return state.staff
-           },
-        GET_STUDENTS(state) {
-        return state.searchedStudent
-        }
-    }
-}
+    async FETCH_ALL_STAFF({ commit }) {
+      const res = await axios.get("auth/v1/all/staff/");
+      commit("SET_ALL_STAFF", res.data.user);
+    },
+
+    async STAFF_DETAIL({ commit }, id) {
+      const res = await axios.get("auth/v1/teacherprofile-detail/" + id + "/");
+      commit("SET_STAFF", res.data);
+    },
+    async UPDATE_USER({ commit }, payload) {
+      const res = await axios.put(
+        "auth/v1/user-detail/" + payload.id + "/",
+        payload
+      );
+    },
+    async UPDATE_PASSWORD({ commit }, payload) {
+      const res = await axios.put("auth/v1/username-update-password/", payload);
+    },
+
+    // search student
+    async FIND_STUDENT({ commit }, payload) {
+      const res = await axios.get("api/v1/search-enroll/", {
+        params: payload,
+      });
+      commit("SET_SAERCHEDSTUDENT", res.data);
+    },
+  },
+  getters: {
+    GET_USER(state) {
+      return state.user;
+    },
+    GET_ME(state) {
+      return state.me;
+    },
+
+    GET_USERS(state) {
+      return state.studentusers
+        .filter((user) => user.is_student === true)
+        .sort((a, b) => a.sur_name.localeCompare(b.sur_name));
+    },
+
+    GET_NOPROFILE_STUDENTS(state) {
+      // return (state.studentusers?.filter(user => user.is_student === true && user.studentprofile === null)) || [];
+      return state.studentusers.filter(
+        (user) => user.is_student == true && user.studentprofile === null
+      );
+    },
+
+    GET_STAFF_USERS(state) {
+      return state.allStaff.filter((user) => user.is_staff == true);
+    },
+    GET_STAFF(state) {
+      return state.staff;
+    },
+    GET_STUDENTS(state) {
+      return state.searchedStudent;
+    },
+  },
+};
