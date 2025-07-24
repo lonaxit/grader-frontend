b8ae257d2fdc2f015e0c5ee95514c2e95ce29022
Tivkaa Manasseh Luper <lupertivkaa@gmail.com>
2025-07-24 19:18:18 +0100

    Update ResultList.vue

--- src/views/grading/ResultList.vue
+++ src/views/grading/ResultList.vue
@@ -1,63 +1,62 @@
 <template>
   <div class="col-sm-12 col-xl-12">
-    <div v-if="loading">
-      <div class="bg-light rounded h-100 p-4">
-        <h6 class="mb-4">RESULT LIST</h6>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">Name</th>
-              <th scope="col">Term</th>
-              <th scope="col">Class</th>
-              <th scope="col">Session</th>
-              <th scope="col">Tot</th>
-              <th scope="col">Avg</th>
-              <th scope="col">Pos</th>
-              <th scope="col">Att</th>
-              <th scope="col">Remove</th>
-              <th scope="col">View</th>
-              <th scope="col">Print</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in results" :key="item.id">
-              <td>
-                {{ item.student_full_name }}
-              </td>
-              <td>{{ item.term_code }}</td>
-              <td>{{ item.class_name }}</td>
-              <td>{{ item.session_name }}</td>
-              <td>{{ item.termtotal }}</td>
-              <td>{{ item.termaverage }}</td>
-              <td>{{ item.termposition }}</td>
-              <td>{{ item.attendance }}</td>
+    <!-- <div v-if="loading"> -->
+    <div class="bg-light rounded h-100 p-4">
+      <h6 class="mb-4">RESULT LIST</h6>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Term</th>
+            <th scope="col">Class</th>
+            <th scope="col">Session</th>
+            <th scope="col">Tot</th>
+            <th scope="col">Avg</th>
+            <th scope="col">Pos</th>
+            <th scope="col">Att</th>
+            <th scope="col">Remove</th>
+            <th scope="col">View</th>
+            <th scope="col">Print</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in results" :key="item.id">
+            <td>
+              {{ item.student_full_name }}
+            </td>
+            <td>{{ item.term_code }}</td>
+            <td>{{ item.class_name }}</td>
+            <td>{{ item.session_name }}</td>
+            <td>{{ item.termtotal }}</td>
+            <td>{{ item.termaverage }}</td>
+            <td>{{ item.termposition }}</td>
+            <td>{{ item.attendance }}</td>
 
-              <td>
-                <button @click="remove(item.id)">Remove</button>
-              </td>
-              <td>
-                <button>view</button>
-                <!-- <router-link
-                  :to="{ name: 'my-detail-result', params: { id: item.id } }"
-                  >Detail</router-link
-                > -->
-              </td>
+            <td>
+              <button @click="remove(item.id)">Remove</button>
+            </td>
+            <td>
+              <router-link
+                :to="{ name: 'my-detail-result', params: { id: item.id } }"
+                >Detail</router-link
+              >
+            </td>
 
-              <td>
-                <router-link
-                  target="_blank"
-                  :to="{ name: 'print-result', params: { id: item.id } }"
-                  >Print</router-link
-                >
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+            <td>
+              <router-link
+                target="_blank"
+                :to="{ name: 'print-result', params: { id: item.id } }"
+                >Print</router-link
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+    <!-- </div>
     <div v-else>
       <p>Loading ...</p>
-    </div>
+    </div> -->
   </div>
 </template>
 
