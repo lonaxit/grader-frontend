add409eb32b7e7b9765d9149b45e7672553313fa
Tivkaa Manasseh Luper <lupertivkaa@gmail.com>
2025-07-24 12:12:34 +0100

    Update ScoresList.vue

--- src/views/grading/ScoresList.vue
+++ src/views/grading/ScoresList.vue
@@ -1,92 +1,82 @@
 <template>
-
   <div class="col-sm-12 col-xl-12">
     <div v-if="loading">
-    
-                        <div class="bg-light rounded h-100 p-4">
-                            <h6 class="mb-4">SCORES</h6>
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Subj</th>
-                                        <th scope="col">Term</th>
-                                        <th scope="col">Class</th>
-                                        <th scope="col">Session</th>
-                                        <th scope="col">F/A</th>
-                                        <th scope="col">S/A</th>
-                                        <th scope="col">T/A</th>
-                                        <th scope="col">EXAM</th>
-                                        <th scope="col">TOTAL</th>
-                                        
-                                        <th scope="col">Remove</th>
-                                 
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr v-for="item in scores" :key="item.id">
-                                        <td>
-                                           {{item.user.sur_name}} {{item.user.first_name}} 
-                                        </td>
-                                        <td>{{item.subject.name}}</td>
-                                        <td>{{item.term.name}}</td>
-                                        <td>{{item.studentclass.class_name}}</td>
-                                        <td>{{item.session.name}}</td>
-                                        <td>{{item.firstscore}}</td>
-                                        <td>{{item.secondscore}}</td>
-                                        <td>{{item.thirdscore}}</td>
-                                        <td>{{item.examscore}}</td>
-                                        <td>{{item.subjecttotal}}</td>
+      <div class="bg-light rounded h-100 p-4">
+        <h6 class="mb-4">SCORES</h6>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Subj</th>
+              <th scope="col">Term</th>
+              <th scope="col">Class</th>
+              <th scope="col">Session</th>
+              <th scope="col">F/A</th>
+              <th scope="col">S/A</th>
+              <th scope="col">T/A</th>
+              <th scope="col">EXAM</th>
+              <th scope="col">TOTAL</th>
 
-                                         <td>
-                                            <button @click="remove(item.id)">
-                                                Remove
-                                            </button>
-                                        
-                                        </td>
-                                  <!-- <td>
+              <th scope="col">Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in scores" :key="item.id">
+              <td>
+                {{ item.student_full_name }}
+              </td>
+              <td>{{ item.subject_code }}</td>
+              <td>{{ item.term_code }}</td>
+              <td>{{ item.class_name }}</td>
+              <td>{{ item.session_name }}</td>
+              <td>{{ item.firstscore }}</td>
+              <td>{{ item.secondscore }}</td>
+              <td>{{ item.thirdscore }}</td>
+              <td>{{ item.examscore }}</td>
+              <td>{{ item.subjecttotal }}</td>
+
+              <td>
+                <button @click="remove(item.id)">Remove</button>
+              </td>
+              <!-- <td>
                                             <router-link  :to="{name:'new-score', params:{id:item.student}}">Add Score</router-link>
                                         </td> -->
-                                 
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div v-else>
+      <p>Loading ...</p>
+    </div>
   </div>
-  <div v-else>
-    <p>Loading ...</p>
-  </div>
-</div>
-
 </template>
 
 <script>
-import {mapActions,mapGetters,mapMutations} from 'vuex'
+import { mapActions, mapGetters, mapMutations } from "vuex";
 export default {
-name:'All Class Teachers',
-data(){
-    return{
-        loading:false
-    }
-},
-computed:{
-...mapGetters({scores:"GET_SCORES"})
-},
-methods:{
-...mapActions(['DELETE_SCORES']),
+  name: "All Class Teachers",
+  data() {
+    return {
+      loading: false,
+    };
+  },
+  computed: {
+    ...mapGetters({ scores: "GET_SCORES" }),
+  },
+  methods: {
+    ...mapActions(["DELETE_SCORES"]),
 
-async remove(id){
-this.DELETE_SCORES(id)
-// this.$router.push('/filter-score/')
-}
-},
+    async remove(id) {
+      this.DELETE_SCORES(id);
+      // this.$router.push('/filter-score/')
+    },
+  },
 
-mounted(){ 
-    this.loading=true
-}
-}
+  mounted() {
+    this.loading = true;
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
