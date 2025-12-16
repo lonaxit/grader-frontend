685747564de6517eb5d0c12c68bb296ae6e15430
Tivkaa Manasseh Luper <78597428+lonaxit@users.noreply.github.com>
2025-12-16 17:43:44 +0100

    Refactor resumption date handling in DetailResult.vue

--- src/views/Student/DetailResult.vue
+++ src/views/Student/DetailResult.vue
@@ -48,10 +48,8 @@
             <td>{{ result.session_name }}</td>
             <td>{{ result.termposition }}</td>
             <td>{{ result.attendance }}</td>
-            <!-- <td>{{ resumption_date[7].current_term_ends }}</td> -->
-            <td>2025-07-25</td>
-            <!-- <td>{{ resumption_date[7].next_term_begins }}</td> -->
-            <td>2025-09-08</td>
+            <td>{{ resumptionEnds }}</td>
+            <td>{{ resumptionNextBegins }}</td>
           </tr>
         </table>
       </div>
@@ -155,15 +153,20 @@ export default {
       affectivetraits: "GET_TRAITS",
       psychotraits: "GET_PSYCHOTRAITS",
       resumedates: "GET_RESUMPTIONS",
+      activeResumption: "GET_ACTIVE_RESUMPTION",
       rollcall: "GET_ENROLLMENTS",
     }),
 
-    resumption_date() {
-      return (this.resumedates = this.resumedates.filter(
-        (item) =>
-          item.current_term === this.result.term.id &&
-          item.session === this.result.session.id
-      ));
+    resumptionEnds() {
+      return this.activeResumption && this.activeResumption.current_term_ends
+        ? this.activeResumption.current_term_ends
+        : "";
+    },
+
+    resumptionNextBegins() {
+      return this.activeResumption && this.activeResumption.next_term_begins
+        ? this.activeResumption.next_term_begins
+        : "";
     },
   },
 
@@ -173,7 +176,7 @@ export default {
       "USER_SCORES_LIST",
       "FETCH_TRAITS",
       "FETCH_PSYCHOTRAITS",
-      "ALL_RESUMPTION",
+        "ACTIVE_RESUMPTION",
       "FETCH_ENROLLMENT",
     ]),
   },
@@ -200,7 +203,7 @@ export default {
             term: this.result.term_id,
           };
           this.FETCH_PSYCHOTRAITS(psychodata).then(() => {
-            this.ALL_RESUMPTION().then(() => {
+            this.ACTIVE_RESUMPTION().then(() => {
               const rollcallpayload = {
                 classroom: this.result.class_id,
                 term: this.result.term_id,
