9871bb67c5c21e0e6ea4ea05648d4b3585db2725
Tivkaa Manasseh Luper <78597428+lonaxit@users.noreply.github.com>
2026-03-14 14:15:19 +0100

    Added functionality to toggle term

--- src/store/modules/term.js
+++ src/store/modules/term.js
@@ -1,48 +1,52 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const term = {
-    state: {
-        term: '',
-        allTerms: [],
+  state: {
+    term: "",
+    allTerms: [],
+  },
+
+  mutations: {
+    SET_TERM(state, payload) {
+      state.term = payload;
     },
 
-    mutations: {
-
-        SET_TERM(state, payload) {
-            state.term = payload
-        },
-
-        SET_TERMS(state, payload) {
-            state.allTerms = payload
-        },
+    SET_TERMS(state, payload) {
+      state.allTerms = payload;
     },
+  },
 
-    actions: {
-        async ADD_TERMS({commit},payload) {
-            const res = await axios.post('api/v1/term/',payload)
-            // commit('SET_TERMS', res.data)     
-        },
-        async ALL_TERMS({commit}) {
-            const res = await axios.get('api/v1/term/')
-            commit('SET_TERMS', res.data)     
-        },
-        async TERM_DETAIL({commit},id) {
-            const res = await axios.get('api/v1/term-detail/' + id +'/')
-              commit('SET_TERM', res.data) 
-        },
-        async UPDATE_TERM({commit},payload) {
-            const res = await axios.put('api/v1/term-detail/' + payload.id +'/',payload)
-            //   commit('SET_TERM', res.data) 
-        }
-       
-        
+  actions: {
+    async ADD_TERMS({ commit }, payload) {
+      const res = await axios.post("api/v1/term/", payload);
+      // commit('SET_TERMS', res.data)
     },
-    getters: {
-        GET_TERMS(state) {
-            return state.allTerms
-        },
-        GET_TERM(state) {
-            return state.term
-        }
-    }
-}
+    async ALL_TERMS({ commit }) {
+      const res = await axios.get("api/v1/term/");
+      commit("SET_TERMS", res.data);
+    },
+    async TERM_DETAIL({ commit }, id) {
+      const res = await axios.get("api/v1/term-detail/" + id + "/");
+      commit("SET_TERM", res.data);
+    },
+    async UPDATE_TERM({ commit }, payload) {
+      const res = await axios.put(
+        "api/v1/term-detail/" + payload.id + "/",
+        payload,
+      );
+      //   commit('SET_TERM', res.data)
+    },
+    async TOGGLE_TERM_STATUS({ commit, dispatch }, id) {
+      await axios.post("api/v1/toggle-term/" + id + "/");
+      dispatch("ALL_TERMS");
+    },
+  },
+  getters: {
+    GET_TERMS(state) {
+      return state.allTerms;
+    },
+    GET_TERM(state) {
+      return state.term;
+    },
+  },
+};
